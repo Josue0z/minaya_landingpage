52d5769a5abc8aff56066411c1f7a951285cbfaf
Josue0z <josue.dev03@gmail.com>
2026-02-27 19:20:09 -0400

    upload

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-
+// Definir la carpeta pública 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('PORT',process.env.PORT || 3000);
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'views','index.html'))
