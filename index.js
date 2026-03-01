b504a6501233e03e01b8d0f3e0752285e98ba603
Josue0z <josue.dev03@gmail.com>
2026-02-28 21:26:48 -0400

    upload changes

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-
+require('dotenv/config')
 
 const express = require('express')
 const path = require('path')
 const app = express();
+
+const nodemailer = require('nodemailer');
+
+// Configuración del transporte
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD
+    }
+});
+
+
 // Definir la carpeta pública 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
 app.set('PORT',process.env.PORT || 3000);
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'views','index.html'))
 })
 
+app.post('/submit',(req,res) =>{
+     try{
+        const {name,subject,email,description} = req.body;
+        transporter.sendMail({
+           from:process.env.GMAIL_USERNAME,
+           to:email,
+           subject: `Nuevo mensaje: ${subject}`,
+           text: `
+                Nombre: ${name}
+                Email: ${email}
+                Asunto: ${subject}
+                Mensaje: ${description}
+            
+            `
+        })
+
+        console.log(req.body)
+        res.json(req.body)
+     }catch(error){
+        res.status(501).json({
+            error
+        })
+     }
+})
 app.listen(app.get('PORT'), (req,res)=>{
     console.log(`server running on port ${app.get('PORT')}`)
 })
